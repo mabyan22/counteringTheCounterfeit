46043851cc6da904c22f5e5b66fb0f3c6e132f17
mabyan22 <m.abyan22@gmail.com>
2022-08-02 16:49:28 +0500

    Fixed backend.py code

--- backend.py
+++ backend.py
@@ -3,8 +3,10 @@ import urllib.request, urllib.parse, urllib.error
 import json
 import ssl
 
-api_key = "397e47df3ec6469e9f6244f8453ed6c7"
-service_url = "https://newsapi.org/v2/everything?"
+apiKey = "397e47df3ec6469e9f6244f8453ed6c7"
+serviceUrl = "https://newsapi.org/v2/everything?"
+
+listOfItems = ["author","title","description","url","urlToImage","publishedAt"]
 
 # Ignore SSL certificate errors
 checkCert = ssl.create_default_context()
@@ -14,57 +16,52 @@ checkCert.verify_mode = ssl.CERT_NONE
 searchStr= str(input("Enter search string: "))
 urlParameters = dict()
 urlParameters["q"]= searchStr
-urlParameters['apiKey']=api_key 
-url = service_url + urllib.parse.urlencode(urlParameters)
+urlParameters['apiKey']=apiKey 
+url = serviceUrl + urllib.parse.urlencode(urlParameters)
 
 print('Retrieving', url)
-uh = urllib.request.urlopen(url, context=checkCert)
-data = uh.read().decode()
+urlHandle = urllib.request.urlopen(url, context=checkCert)
+data = urlHandle.read().decode()
 print('Retrieved', len(data), 'characters')
 
 try:
-    js = json.loads(data)
+    jsDict = json.loads(data)
 except:
-    js = None
+    jsDict = None
 retry=0
 while retry<3:
-    if (not js or 'status' not in js or js['status']!='ok') :
+    if (not jsDict or 'status' not in jsDict or jsDict['status']!='ok') :
         time.sleep(2) 
         retry=retry+1
-        uh = urllib.request.urlopen(url, context=checkCert)
-        data = uh.read().decode()
+        urlHandle = urllib.request.urlopen(url, context=checkCert)
+        data = urlHandle.read().decode()
         print('Retrieved', len(data), 'characters')
         try:
-            js = json.loads(data)
+            jsDict = json.loads(data)
         except:
-            js = None
+            jsDict = None
         continue
     else: break 
 
-if not js or 'status' not in js or js['status'] != 'ok':
-    print('==== Failure To Retrieve ====')
-else:
-    print("Total results: ", js["totalResults"])
+if 'status' in jsDict and 'status' in jsDict and jsDict['status'] == 'ok':
+    print("Total results: ", jsDict["totalResults"])
     responses=dict()
     #responses[i]=[name,author,title,description,url,urlToImage,publishedAt]
-    if js["totalResults"]<5:
-        noOfResults = int(js["totalResults"])
+    if jsDict["totalResults"]<5:
+        noOfResults = int(jsDict["totalResults"])
     else:
         noOfResults=5
     for i in range(noOfResults):
-        responses[i]=[""]*7
-        responses[i][0]=js["articles"][i]["source"]["name"]
-        responses[i][1]=js["articles"][i]["author"]
-        responses[i][2]=js["articles"][i]["title"]
-        responses[i][3]=js["articles"][i]["description"]
-        responses[i][4]=js["articles"][i]["url"]
-        responses[i][5]=js["articles"][i]["urlToImage"]
-        responses[i][6]=js["articles"][i]["publishedAt"]
-    
-for i in range(noOfResults):
-    for k in range(7):
-        print(responses[i][k])
-
-
-
-
+        for results in range(7):
+            if results != 0 :
+                for item in listOfItems:
+                    print(listOfItems.index(item))
+                    responses[i][(listOfItems.index(item))+1] = jsDict["articles"][i][item]
+            else:
+                responses[i]=[""]*7
+                responses[i][0]=jsDict["articles"][i]["source"]["name"]
+    for j in range(noOfResults):
+        for k in range(7):
+            print(responses[i][k])   
+else:
+    print('==== Failure To Retrieve ====')
