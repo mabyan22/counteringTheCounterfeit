5389714e4cbb671f7c3feeb0c9c27c53273ad2aa
Mohammad Owais Kerney <owaiskerney@Mohammads-MacBook-Pro.local>
2022-08-05 22:28:00 +0800

    Frontend initial setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React ,{useState,useEffect}from 'react'
+import "./App.css";
 
-function App() {
-  const[data,setData]=useState([{}])
-
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
-  },[])
-  return (
-    <div>
-
-      {(typeof data.members === 'undefined')? (
-        <p>Loading...</p>
-      ):(
-        data.members.map((member,i)=>(
-          <p key={i}>{member}</p>
-        ))
-      )}
-      
-    </div>
-  )
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { Loading } from "./components/Loading";
+//react-router
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+export default class App extends Component {
+  render() {
+    return (
+      <>
+       <Navbar />
+        <Router>
+    
+          <Switch>
+          
+            <Route exact path="/">
+              <News key={"general"} pageSize={9} country="in" category="general" />
+            </Route>
+            <Route exact path="/Business">
+              <News key={"business"} pageSize={9} country="in" category="business" />
+            </Route>
+            <Route exact path="/Science">
+              <News key={"science"} pageSize={9} country="in" category="science" />
+            </Route>
+            <Route exact path="/Entertainment">
+              <News key={"entertainment"} pageSize={9} country="in" category="entertainment" />
+            </Route>
+            <Route exact path="/Sports">
+              <News key={"sports"} pageSize={9} country="in" category="sports" />
+            </Route>
+            <Route exact path="/Tech">
+              <News key={"tech"} pageSize={9} country="in" category="technology" />
+            </Route>
+            <Route exact path="/Health">
+              <News  key={"health"} pageSize={9} country="in" category="health" />
+            </Route>
+          </Switch>
+        </Router>
+      </>
+    );
+  }
 }
-
-export default App 
